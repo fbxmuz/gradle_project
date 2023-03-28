9e31af6eb7c1c00388b37ea8ce685ce274a6ba43
David Harris <davidharrisnet@gmail.com>
2023-03-27 21:06:30 -0400

    working with alg4.jar third-party.  Deleted the modules file

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,10 +1,12 @@
 package hello;
 
-
+import edu.princeton.cs.algs4.UF;
 
 public class HelloWorld {
   public static void main(String[] args) {
   Greeter greeter = new Greeter();
   System.out.println(greeter.sayHello());
+  UF uf = new UF(2);
+  System.out.println(uf.count());
   }
 }
--- src/main/java/module-info.java
+++
@@ -1,3 +0,0 @@
-module gradle_project {
-	exports hello;
-}
